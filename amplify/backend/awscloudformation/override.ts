e6dba5bd50bcccdd1e5981e6576e63eafaf0bc43
xin.jin <xin.jin@smartnews.com>
2022-02-07 16:40:10 +0800

    feat:override

--- amplify/backend/awscloudformation/override.ts
+++ amplify/backend/awscloudformation/override.ts
@@ -1,6 +1,3 @@
 import { AmplifyRootStackTemplate } from "@aws-amplify/cli-extensibility-helper";
 
-export function override(resources: AmplifyRootStackTemplate) {
-  resources.authRole.roleName = "se-us-fe-sa";
-  resources.authRole.path = "/smartnews/US-Engagement/";
-}
+export function override(resources: AmplifyRootStackTemplate) {}
